776efdc24041713de19909f388beab1a1a2d7306
Umair Ali <aliumair14800@gmail.com>
2026-03-16 12:29:46 +0500

    Add base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ali-umair.github.io',
+  base: '/ali-umair/',
   vite: {
     plugins: [tailwindcss()]
   }
